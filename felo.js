2c79be8a0758cbb68a3f79c55ff9cd827a465bde
Shjingit <165047639+Shjingit@users.noreply.github.com>
2026-07-05 13:13:14 +0800

    Update felo.js

--- felo.js
+++ felo.js
@@ -33,8 +33,8 @@ chxm1023 = {
         "deduction_type" : "DURATIONS",
         "user_id" : "1777777777777777777",
         "product_item_type" : "TRANSLATOR_BENEFIT",
-        "total" : 0,
-        "balance" : 0,
+        "total" : 1800,
+        "balance" : 1000,
         "user_product_total_id" : "1732077752393142273"
       }
     ],
