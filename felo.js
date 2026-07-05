5e83917b9359b901a4e2fb9547a8b7e02d6a0c4d
Shjingit <165047639+Shjingit@users.noreply.github.com>
2026-07-05 12:40:55 +0800

    Update felo.js

--- felo.js
+++ felo.js
@@ -31,11 +31,11 @@ chxm1023 = {
         "freeze" : 0,
         "total_limit" : "UNLIMITED",
         "deduction_type" : "DURATIONS",
-        "user_id" : "1777777777777777777",
+       /* "user_id" : "1777777777777777777",*/
         "product_item_type" : "TRANSLATOR_PRO_ANNUAL",
         "total" : 1800,
         "balance" : 1800,
-        "user_product_total_id" : "1732077752393142273"
+       /* "user_product_total_id" : "1732077752393142273"*/
       }
     ],
     "type" : "PRO"
