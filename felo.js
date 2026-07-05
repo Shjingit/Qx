fa989a3bb179d97c25c6ada919d4a910d1c5f7be
Shjingit <165047639+Shjingit@users.noreply.github.com>
2026-07-05 13:07:07 +0800

    Update felo.js

--- felo.js
+++ felo.js
@@ -23,22 +23,22 @@ var chxm1023 = JSON.parse($response.body);
 chxm1023 = {
   "status" : 200,
   "data" : {
-    "active_time" : "2026-06-05T16:41:31+0000",
+    "active_time" : "2023-12-05T16:41:31+0000",
     "expire_time" : "2099-09-09T09:09:09+0000",
-    "now" : "2026-06-05T16:42:06+0000",
+    "now" : "2023-12-05T16:42:06+0000",
     "user_product_total" : [
       {
         "freeze" : 0,
-        "total_limit" : "LIMITED",
+        "total_limit" : "UNLIMITED",
         "deduction_type" : "DURATIONS",
-       /* "user_id" : "1777777777777777777",*/
-        "product_item_type" : "TRANSLATOR_DURATIONS",
-        "total" : 1800,
-        "balance" : 1800
-       /* "user_product_total_id" : "1732077752393142273"*/
+        "user_id" : "1777777777777777777",
+        "product_item_type" : "TRANSLATOR_BENEFIT",
+        "total" : 0,
+        "balance" : 0,
+        "user_product_total_id" : "1732077752393142273"
       }
     ],
-    "type" : "FREE"
+    "pro" : true
   },
   "code" : "OK"
 };
