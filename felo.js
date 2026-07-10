398ce9f4ac9c1e6a64799e5c1e0ede1473594720
Shjingit <165047639+Shjingit@users.noreply.github.com>
2026-07-10 12:24:46 +0800

    Update felo.js

--- felo.js
+++ felo.js
@@ -10,47 +10,54 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/translator\.felo\.me\/api\/user\/plan url script-response-body https://raw.githubusercontent.com/Shjingit/Qx/refs/heads/main/felo.js
+^https://api.waxueshe.com/api/v2/users/my url script-response-body https://raw.githubusercontent.com/Shjingit/Qx/refs/heads/main/felo.js
 
 [mitm]
-hostname = translator.felo.me
+hostname = api.waxueshe.com
 
 *************************************/
 
 
 var chxm1023 = JSON.parse($response.body);
 
-chxm1023 = {
-  
-  "status" : 200,
+chxm1023 ={
+  "msg" : "ok",
   "data" : {
-    "type" : "FREE",
-    "user_product_total" : [
-      {
-        "freeze" : 0,
-        "total_limit" : "UNLIMITED",
-        "deduction_type" : "",
-        "user_id" : "1732233102102020097",
-        "product_item_type" : "TRANSLATOR_BENEFIT_FREE",
-        "total" : 3600,
-        "balance" : 3600,
-        "user_product_total_id" : "1732669700107669505"
-      },
-      {
-        "freeze" : 0,
-        "total_limit" : "LIMITED",
-        "deduction_type" : "",
-        "user_id" : "1732233102102020097",
-        "product_item_type" : "TRANSLATOR_DURATIONS",
-        "total" : 1800,
-        "balance" : 1761,
-        "user_product_total_id" : "1732669700493545473"
-      }
-    ],
-    "now" : "2026-07-05T08:56:18+0000"
+    "mobile_tip" : 0,
+    "is_get_free" : 1,
+    "ise_use_total" : 10,
+    /*"free_time" : "0秒",
+    "experience_time" : 5742,*/
+    "user_id" : 272315,
+    "invitation_code" : "OVDxoMaX",
+    "head_img" : "",
+    "wechat_official_account" : "oHQe87VGKLHt7LomLfDwUteiBwm0",
+    "vip_type" : 5,
+    "remain_time" : "2026-07-30 09:09:09",
+    "nickname" : "Shjin",
+    "phone_tip" : 1,
+    "is_collect_recording" : 1,
+    "signature" : "这个学员很神秘~还没有留下签名...",
+    /*"vip_info" : {
+
+    },
+    "promotion_record" : {
+
+    },*/
+    "email" : "",
+    "has_parent" : true,
+    "wechat_id" : "",
+    "pc_tip" : 0,
+    "is_get_fee_tip" : 2,
+    "phone" : "15170266963",
+    "created_at" : 1783335176,
+    "diy_code" : "",
+    "ise_use_today_total" : 0,
+    "superior_wechat_id" : "h9867620"
   },
-  "code" : "OK"
+  "code" : 0
 }
+
 
 
 
